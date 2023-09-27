e5dcad8196b548bf5c8c62c1d122db9cee83548d
Siddhartha45 <87509046+Siddhartha45@users.noreply.github.com>
2023-09-27 23:11:33 +0545

    changed database

--- fpn/settings.py
+++ fpn/settings.py
@@ -79,10 +79,24 @@ WSGI_APPLICATION = 'fpn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# Default database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Postgress database for server
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pme',      
+        'USER': 'kantipur',
+        'PASSWORD': 'kantipur',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
